4a94b94b3e7448f57b9dcc38c28a862f343acd10
predictmaths <143323985+predictmaths@users.noreply.github.com>
2023-08-27 23:45:42 +0530

    Update equationsolver.py

--- pages/equationsolver.py
+++ pages/equationsolver.py
@@ -1,10 +1,9 @@
 #Complex Word Math Problem Solver App
 import streamlit as st
 from langchain import OpenAI, LLMMathChain
-from apikey import apikey
 import os
 
-os.environ["OPENAI_API_KEY"] = apikey
+os.environ["OPENAI_API_KEY"] = ""
 llm = OpenAI(temperature=0)
 llm_math = LLMMathChain.from_llm(llm, verbose=True)
 
